16c47d7f26b5565aea64fe7304ff10780558d0d0
竹村 光 <takemura@dl.kuis.kyoto-u.ac.jp>
2012-08-05 14:13:24 +0900

    8/5

---
+++ exercise1-1/lib/TemplateEngine.pm
@@ -0,0 +1,36 @@
+package TemplateEngine;
+
+use strict;
+use warnings;
+use utf8;
+
+sub new {
+    my ($class, %args) = @_;
+    my $self = { %args };
+    bless $self, $class;
+    return $self;
+}
+
+sub render {
+    my ($self, $data) = @_;
+    open(IN, $self->{file}) or die("Can't open $self->{file}");
+    my $template;
+    while (my $line = <IN>){
+        while ((my $key, my $val) = each (%$data)) {
+            if (index($line, '{% $key %}')){
+                my $str = $val;
+                $str =~ s/&/&amp;/g;
+                $str =~ s/</&lt;/g;
+                $str =~ s/>/&gt;/g;
+                $str =~ s/"/&quot;/g;
+                $line =~ s/{% $key %}/$str/;
+            }
+        }
+        utf8::encode($line);
+        #$template .= $line;
+    }
+    return $template;
+}
+
+1;
+__END__
---
+++ exercise1-1/t/template.t
@@ -0,0 +1,28 @@
+use strict;
+use warnings;
+
+use Test::More;
+use FindBin::libs;
+
+use_ok 'TemplateEngine';
+
+my $template = TemplateEngine->new( file => '../templates/main.html' );
+isa_ok $template, 'TemplateEngine';
+
+my $expected = <<'HTML';
+<html>
+  <head>
+    <title>タイトル</title>
+  </head>
+  <body>
+    <p>これはコンテンツです。&amp;&lt;&gt;&quot;</p>
+  </body>
+</html>
+HTML
+
+cmp_ok $template->render({
+    title   => 'タイトル',
+    content => 'これはコンテンツです。&<>"',
+}), 'eq', $expected;
+
+done_testing();
