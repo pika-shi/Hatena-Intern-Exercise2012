9f9740f9b4cc7b300d35c2e339eeec9085c14ec6
竹村 光 <takemura@dl.kuis.kyoto-u.ac.jp>
2012-08-05 11:32:03 +0900

    Revert "Revert "Revert "8/4"""
    This reverts commit 1686fb8999ef593b841f516d4a8495c488d75351.

--- exercise2-1/template.js
+++ exercise2-1/template.js
@@ -1,17 +1,9 @@
 var Template = function(input) {
-    this.input = input;
+    // この関数を実装してください
 };
 
 Template.prototype = {
     render: function(variables) {
-        source = this.input.source
-        for(var i in variables){
-            str = variables[i];
-            str = str.replace(/&/g,'&amp;');
-            str = str.replace(/>/g,'&gt;');
-            str = str.replace(/</g,'&lt;');
-            source = source.replace('{% ' + i + ' %}', str);
-        }
-        return source;
+        // この関数を実装してください
     }
 };
